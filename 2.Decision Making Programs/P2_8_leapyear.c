b998405a335c040d79fdf03723b8a2af6e8b82ed
Kashyap Patel <46kashyappatel@gmail.com>
2026-07-08 13:18:08 +0530

    Add files via upload

---
+++ 2.Decision Making Programs/P2_8_leapyear.c
@@ -0,0 +1,16 @@
+//Program to check whether a year is leap year or not.
+#include<stdio.h>
+int main()
+{
+    int year;
+
+    printf("Enter the year:");
+    scanf("%d" , &year);
+
+    if(year%400==0 || (year%4==0 && year%100!=0))
+    printf("%d is a leap year." , year);
+    else
+    printf("%d is a not leap year." , year);
+
+    return 0;
+}
